7e7121ddba569a740d6a47bb1a68ac8726ad33d2
huyanli <610796017@qq.com>
2019-01-25 16:44:04 +0800

    初始化项目

---
+++ Scripts/test_01_001.py
@@ -0,0 +1,9 @@
+class Test:
+
+    def test_01_001(self):
+        print("-----test_01_001")
+        assert True
+
+    def test_02_002(self):
+        print("-----test_02_002")
+        assert False
